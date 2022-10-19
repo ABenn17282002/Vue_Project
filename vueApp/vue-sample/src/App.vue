b9e2b25b5fd0ed5a21bcc20b2cbd67aa32a66cbf
ABenn17282002 <a.benn17282002@gmail.com>
2022-10-19 06:38:15 +0000

    Vue2Router(router_link:props)

--- vueApp/vue-sample/src/App.vue
+++ vueApp/vue-sample/src/App.vue
@@ -1,23 +1,13 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <TestComponent></TestComponent>
-    <HelloWorld msg="Welcome" />
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <!-- <router-link to="/about" tag="button">About</router-link> -->
+      <router-link to="/about" exact-active-class="test">About</router-link>
+    </nav>
+    <router-view />
   </div>
 </template>
-
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
-import TestComponent from "./module/TestComponent.vue";
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-    TestComponent,
-  },
-};
-</script>
 
 <style>
 #app {
@@ -26,6 +16,27 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-active {
+  color: red;
+}
+
+/* active状態でのクラス */
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+nav a.test {
+  color: lightblue;
 }
 </style>
