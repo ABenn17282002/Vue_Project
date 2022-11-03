45d63530ff56d2b9ef83314159c1b7ae4a9efc31
ABenn17282002 <a.benn17282002@gmail.com>
2022-11-03 05:07:02 +0000

    Vue2_Vuetify(Heaeder&Footer)

---
+++ vueApp/vue-sample/src/global/HeaderView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center"></div>
+
+      <v-spacer></v-spacer>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style></style>
