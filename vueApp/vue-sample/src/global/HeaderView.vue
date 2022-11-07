6f0065916fd4c995253466e63617ad108895a055
ABenn17282002 <a.benn17282002@gmail.com>
2022-11-07 07:04:54 +0000

    Vue2Router(LocalStorageDelete)

--- vueApp/vue-sample/src/global/HeaderView.vue
+++ vueApp/vue-sample/src/global/HeaderView.vue
@@ -4,12 +4,19 @@
       <div class="d-flex align-center"></div>
 
       <v-spacer></v-spacer>
+      <v-btn color="error" @click="deleteLocalStorage">削除する</v-btn>
     </v-app-bar>
   </div>
 </template>
 
 <script>
 export default {
+  name: "HeaderView",
+  methods: {
+    deleteLocalStorage() {
+      this.$emit("delete-local-storage");
+    },
+  },
 };
 </script>
 
