983e74f5a5d4db8fb0f8790b11aec517428b7227
ABenn17282002 <a.benn17282002@gmail.com>
2022-11-08 02:10:06 +0000

    Vue2Vuex(initSetting)

--- vueApp/vue-sample/src/App.vue
+++ vueApp/vue-sample/src/App.vue
@@ -1,23 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <TestComponent></TestComponent>
-    <HelloWorld msg="Welcome" />
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
-import TestComponent from "./module/TestComponent.vue";
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-    TestComponent,
-  },
-};
-</script>
 
 <style>
 #app {
@@ -26,6 +15,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
