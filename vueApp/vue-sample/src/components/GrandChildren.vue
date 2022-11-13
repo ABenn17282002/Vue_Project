f875dead64dab5537a3f4667272571144f5a41e1
ABenn17282002 <a.benn17282002@gmail.com>
2022-11-13 14:22:43 +0000

    Vue3(Provide/Inject)

---
+++ vueApp/vue-sample/src/components/GrandChildren.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    GrandChild(孫)
+
+    <div>{{ userName }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {},
+  inject: ["userName"],
+};
+</script>
+
+<style></style>
