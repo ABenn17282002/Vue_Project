0935de28b04a55e267a367fe19214b166309049c
ABenn17282002 <a.benn17282002@gmail.com>
2022-11-14 14:10:38 +0000

    Vue3(CompositionAPI・setup)

---
+++ vueApp/vue-sample/src/views/CompositionTest.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>CompositionTest</div>
+</template>
+
+<script>
+export default {
+  data() {},
+  setup() {
+    console.log("setup");
+    // console.logで出力するとundefinedになる(thisは使えない)
+    console.log(this);
+  },
+  created() {
+    console.log("created");
+    console.log(this);
+  },
+  mounted() {
+    console.log("mounted");
+  },
+};
+</script>
+
+<style></style>
