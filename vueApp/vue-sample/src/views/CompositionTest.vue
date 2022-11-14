6e1efe30181ec0764a530abae14793dc84fbd9dc
ABenn17282002 <a.benn17282002@gmail.com>
2022-11-14 14:24:57 +0000

    Vue3(setupの戻り値)

--- vueApp/vue-sample/src/views/CompositionTest.vue
+++ vueApp/vue-sample/src/views/CompositionTest.vue
@@ -1,14 +1,30 @@
 <template>
-  <div>CompositionTest</div>
+  <div>
+    CompositionTest
+    <p>{{ name }}</p>
+    <p>{{ age }}</p>
+  </div>
 </template>
 
 <script>
 export default {
-  data() {},
   setup() {
+    let name = "水谷";
+    const age = 32;
     console.log("setup");
     // console.logで出力するとundefinedになる(thisは使えない)
     console.log(this);
+    return {
+      // name:nameではなくnameだけの省略形も可。
+      name,
+      age,
+    };
+  },
+  data() {
+    return {
+      number: 1,
+      sports: "サッカー",
+    };
   },
   created() {
     console.log("created");
