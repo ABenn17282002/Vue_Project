ab5d4fba1abc9220bb8d8d13fc1ece3c885dfb29
ABenn17282002 <a.benn17282002@gmail.com>
2022-11-15 08:39:00 +0000

    Vue3(ref())

--- vueApp/vue-sample/src/views/CompositionTest.vue
+++ vueApp/vue-sample/src/views/CompositionTest.vue
@@ -3,21 +3,28 @@
     CompositionTest
     <p>{{ name }}</p>
     <p>{{ age }}</p>
+    <p>{{ nameRef }}</p>
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
   setup() {
     let name = "水谷";
     const age = 32;
+    const nameRef = ref("野茂");
+
     console.log("setup");
     // console.logで出力するとundefinedになる(thisは使えない)
     console.log(this);
+    console.log(nameRef);
+
     return {
       // name:nameではなくnameだけの省略形も可。
       name,
       age,
+      nameRef,
     };
   },
   data() {
