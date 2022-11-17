9b2d8e3dcef3baa58732f19c0956280841407c14
ABenn17282002 <a.benn17282002@gmail.com>
2022-11-17 05:11:31 +0000

    Vue3(setup(VueX))

---
+++ vueApp/vue-sample/src/views/VuexTest.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    VuexTest
+
+    <div>{{ count }}</div>
+    <button @click="increment">+</button>
+  </div>
+</template>
+
+<script>
+import { computed } from "@vue/reactivity";
+import { useStore } from "vuex";
+
+export default {
+  setup() {
+    const store = useStore();
+
+    const count = computed(() => {
+      return store.state.count;
+    });
+
+    const increment = () => {
+      store.commit("increment");
+    };
+
+    return { count, increment };
+  },
+};
+</script>
+
+<style></style>
